ebea7961983e89abf09dd3d47a0c9188edcca22a
Alisson <alisson.souz.pa@gmail.com>
2022-05-19 16:02:49 -0300

    items of array

--- src/data/items.ts
+++ src/data/items.ts
@@ -18,7 +18,7 @@ export const items =[
         value: 330.00
     },
     {
-        date: new Date(2022, 4, 11),
+        date: new Date(2022, 4, 11  ),
         category: 'food',
         tittle: 'McDonalds',
         value: 30.00

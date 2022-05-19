c83e9b77d0e7620bc51301dd2506ad58cff13e0b
Alisson <alisson.souz.pa@gmail.com>
2022-05-19 16:01:40 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,26 @@
+export const items =[
+    {
+        date: new Date(2022, 4, 15),
+        category: 'food',
+        tittle: 'McDonalds',
+        value: 30.00
+    },
+    {
+        date: new Date(2022, 4, 16),
+        category: 'food',
+        tittle: 'McDonalds',
+        value: 32.00
+    },
+    {
+        date: new Date(2022, 3, 1),
+        category: 'food',
+        tittle: 'McDonalds',
+        value: 330.00
+    },
+    {
+        date: new Date(2022, 4, 11),
+        category: 'food',
+        tittle: 'McDonalds',
+        value: 30.00
+    },
+]
